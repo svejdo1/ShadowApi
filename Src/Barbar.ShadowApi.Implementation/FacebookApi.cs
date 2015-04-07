76a928a7c0d180997ea215b348d5b7fb914f3483
Ondrej Svejdar <svejdo1@gmail.com>
2015-04-07 22:57:49 +0200

    generic credentials

--- Src/Barbar.ShadowApi.Implementation/FacebookApi.cs
+++ Src/Barbar.ShadowApi.Implementation/FacebookApi.cs
@@ -20,8 +20,8 @@ namespace Barbar.ShadowApi.Implementation
       {
         try
         {
-          browser.TextField(Find.ByName("email")).Value = "svejdo1@gmail.com";
-          browser.TextField(Find.ByName("pass")).Value = "fb@jackal12";
+          browser.TextField(Find.ByName("email")).Value = "your_facebook_email";
+          browser.TextField(Find.ByName("pass")).Value = "your_facebook_password";
           browser.Form(Find.ById("login_form")).Submit();
           browser.WaitForComplete();
         }
